0b836dc4c28c49d15942441847173d6d5b7d3a0c
ShowoffTH <95130098+Idklol55@users.noreply.github.com>
2022-09-05 15:42:04 +0700

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1166,7 +1166,7 @@ class PlayState extends MusicBeatState
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
 				if (curDifficulty == 0)
-				case 'Nyeh Heh Heh | Bonely One' | 'Not Enough'':
+				case 'Nyeh Heh Heh | Bonely One' | 'Not Enough':
 					startDialogue(dialogueJson);
 				end
 					
