3f2348e9535c184a837d5dae0a70b07ce9395144
ShowoffTH <95130098+Idklol55@users.noreply.github.com>
2022-09-06 21:47:38 +0700

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -909,13 +909,13 @@ class PlayState extends MusicBeatState
 							}
 						});
 					});
-					if (storyDifficulty == 0)
-					case 'Nyeh Heh Heh' | 'Bonely One' | 'Not Enough':
-						startDialogue(dialogueJson);
-
 					case 'senpai' | 'roses' | 'thorns':
 						if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 						schoolIntro(doof);
+
+					if (storyDifficulty == 0)
+					case 'Nyeh Heh Heh' | 'Bonely One' | 'Not Enough':
+						startDialogue(dialogueJson);
 
 				default:
 					startCountdown();
