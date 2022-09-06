d8b4212ffcb7ec14a33bba5287c73ef9f51d9776
ShowoffTH <95130098+Idklol55@users.noreply.github.com>
2022-09-06 20:07:58 +0700

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -100,7 +100,7 @@ class MainMenuState extends MusicBeatState
 		characters.animation.addByPrefix('freeplay', 'freeplay', 24);
 		characters.animation.addByPrefix('credits', 'credits', 24);
 		characters.animation.addByPrefix('options', 'options', 24);
-		characters.setGraphicSize(Std.int(characters.width * 1));
+		characters.setGraphicSize(Std.int(characters.width * 1.02));
 		characters.scrollFactor.set(1, 1);
 		add(characters);
 		
@@ -174,7 +174,7 @@ class MainMenuState extends MusicBeatState
 
 		#if android
 		addVirtualPad(UP_DOWN, A_B);
-		_virtualpad.y = -46;
+		_virtualpad.y = -47;
 		#end
 
 		super.create();
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -909,6 +909,7 @@ class PlayState extends MusicBeatState
 							}
 						});
 					});
+
 				if (storyDifficulty == 0)
 					case 'Nyeh Heh Heh' | 'Bonely One' | 'Not Enough':
 						startDialogue(dialogueJson);
@@ -916,6 +917,7 @@ class PlayState extends MusicBeatState
 					case 'senpai' | 'roses' | 'thorns':
 						if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 						schoolIntro(doof);
+
 				default:
 					startCountdown();
 			}
